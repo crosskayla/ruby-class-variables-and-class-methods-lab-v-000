f0cb22400aa34c388ee80d47caee9f6dcb32aad0
Kayla Cross <kac288@georgetown.edu>
2018-03-20 16:40:55 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,12 +4,10 @@ class Song
   
   attr_accessor :name, :artist, :genre, :count
   
-  @@count = 
+  @@count = 0
   
   def initialize(song_name, song_artist, song_genre)
     @name, @artist, @genre = song_name, song_artist, song_genre
     @@count += 1
   end
 end
-
-binding.pry
