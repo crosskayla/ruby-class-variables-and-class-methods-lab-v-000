e16831d700bac4898e99edf8e4fdba8fa7b3094c
Kayla Cross <kac288@georgetown.edu>
2018-03-20 16:46:32 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Song
   
-  attr_accessor :name, :artist, :genre, :artists, :genres
+  attr_accessor :name, :artist, :genre, :genres
   
   @@count = 0
   
@@ -12,12 +12,14 @@ class Song
   def initialize(song_name, song_artist, song_genre)
     @name, @artist, @genre = song_name, song_artist, song_genre
     @@count += 1
-    @@artists << @artist
+    @@artists << @artist unless @@artists.include()
     @@genres << @genre
   end
   
   def self.count
     @@count
   end
+  
+  def self.artists
   
 end
